d370dabb10f93d95fae4e0c6ea6b84f9a251a0d5
Saulo Germano <sgwebr@gmail.com>
2025-02-21 14:45:14 -0300

    Fix APIs

--- config/functions.php
+++ config/functions.php
@@ -171,10 +171,11 @@ function instalar($db_ip, $db_user, $db_pass, $db_name, $db_data, $l2jruss, $adm
 		array(4, 'TOPGS200', 'http://www.topgs200.com', 'topgs200.jpg', 'topgs200.php', 'sem_id', 'sem_token', 0, 0),
 		array(5, 'Top 100 Arena', 'http://www.top100arena.com', 'top100arena.jpg', 'top100arena.php', 'sem_id', 'sem_token', 0, 0),
 		array(6, 'GameBytes', 'https://www.gamebytes.net', 'gamebytes.png', 'gamebytes.php', 'sem_id', 'sem_token', 0, 0),
-		array(7, 'L2Top', 'https://l2top.co/', 'l2top.co.png', 'l2top.co.php', 'sem_id', 'sem_token', 0, 0),
+		array(7, 'L2Top', 'https://l2top.co', 'l2top.co.png', 'l2top.co.php', 'sem_id', 'sem_token', 0, 0),
 		array(8, 'L2 Votes', 'https://www.l2votes.com', 'l2votes.jpg', 'l2votes.php', 'sem_id', 'sem_token', 1, 0),
 		array(9, 'iTopZ', 'https://itopz.com', 'itopz.png', 'itopz.php', 'sem_id', 'sem_token', 1, 0),
-		array(10, 'HOP ZONE EU', 'https://hopzone.eu', 'hopzoneu.png', 'hopzoneu.php', 'sem_id', 'sem_token', 1, 0)
+		array(10, 'HOP ZONE EU', 'https://hopzone.eu', 'hopzoneu.png', 'hopzoneu.php', 'sem_id', 'sem_token', 1, 0),
+		array(11, 'GamesTop200', 'https://www.gamestop200.com', 'gamestop200.jpg', 'gamestop200.php', 'sem_id', 'sem_token', 1, 0)
 	);
 if (empty($admins)) {
     return respostaDelay($language_72, 4000);
