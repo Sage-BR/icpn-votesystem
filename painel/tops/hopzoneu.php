c9c8693380e4d183cd8da895b06b0b9aa9ba8133
Saulo Germano <sgwebr@gmail.com>
2025-02-24 09:47:44 -0300

    FIx
    Hopzone and itopz

--- painel/tops/hopzoneu.php
+++ painel/tops/hopzoneu.php
@@ -9,52 +9,88 @@
 //       Brazillian Developer / WebSite: http://www.icpfree.com.br       \\
 //                 Email & Skype: ivan1507@gmail.com.br                  \\
 //=======================================================================\\
-//							 4TeamBR Fixes								 \\
+//				      4TeamBR https://4teambr.com/						 \\
 
+// Definir URL da API
 $URL = "https://api.hopzone.eu/v1/?api_key={$row->top_token}&ip=" . get_client_ip() . "&type=json";
+
+// Inicializar cURL
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $URL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_USERAGENT, 'curl/7.68.0 ICPNetwork/2.8');
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+// Executar a requisição e obter resposta
 $response = curl_exec($ch);
+$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-$json = json_decode($response, true);
-$next_vote_time = $json['vote_time'] + 43200; // 12 horas em segundos
-$can_vote = (time() >= $next_vote_time);
+$can_vote = false;
+$tops_voted = array_replace($tops_voted, array($i => array(1, '0000-00-00 00:00:00')));
 
+if ($response === false || $http_status != 200) {
+    // Se a API não responder, permitir o voto como fallback
+    $can_vote = true;
+} else {
+    $json = json_decode($response, true);
+
+    if ($json !== null && isset($json['status']) && $json['status'] === 'completed') {
+        if (isset($json['vote_time'])) {
+            // Capturar o fuso horário do servidor
+            $timezone_server = date_default_timezone_get();
+
+            // Configurar UTC para processar vote_time corretamente
+            date_default_timezone_set('Europe/Athens'); // Exemplo de GMT+3
+
+            // Converter vote_time (Unix Timestamp) para UTC
+            $vote_time_utc = date("Y-m-d H:i:s", $json['vote_time']);
+
+            // Ajustar para o fuso horário do servidor
+            date_default_timezone_set($timezone_server);
+            $vote_time_local = date("Y-m-d H:i:s", strtotime($vote_time_utc));
+
+            // Somar 12 horas ao horário local do voto
+            $next_vote_time = date("Y-m-d H:i:s", strtotime($vote_time_local . " +12 hours"));
+
+            // Capturar o horário atual no fuso do servidor
+            $current_time = date("Y-m-d H:i:s");
+
+            // Verificar se o tempo atual já passou do próximo voto permitido
+            if ($current_time >= $next_vote_time) {
+                $can_vote = true;
+            } else {
+                $can_vote = false;
+            }
+        } else {
+            $can_vote = true;
+        }
+    } elseif (isset($json['status']) && $json['status'] === 'pending') {
+        $can_vote = true;
+    } else {
+        $can_vote = true;
+    }
+}
+
+// Atualizar o array de votos com o estado atual
+$tops_voted = array_replace($tops_voted, array($i => array($can_vote ? 1 : 0, date('Y-m-d H:i:s'))));
+
+// Exibir botão ou contador de tempo restante
 if ($can_vote):
     ?>
-    <div style="width:87px; height:47px; border:1px solid #999; margin-top:5px; margin-left:5px; float:left;">
-        <a href="https://hopzone.eu/vote/<?php echo $row->top_id; ?>" target="_blank">
-            <img src="images/buttons/<?php echo $row->top_img; ?>" title="HopZone EU" border="0" width="87" height="47">
-        </a>
+    <div style='width:87px; height:47px; border:1px solid #999; margin-top:5px; margin-left:5px; float:left;'>
+        <a href='https://hopzone.eu/vote/<?php echo $row->top_id; ?>' target='_blank'><img src='images/buttons/<?php echo $row->top_img; ?>' title='Top L2JBrasil de Servidores de Lineage2' border='0' width='87' height='47'></a>
     </div>
     <?php
 else:
-    $data_voto = date("Y-m-d H:i:s", $next_vote_time);
-    $data_voto_exploded = explode(" ", $data_voto);
-    $data_partes = explode("-", $data_voto_exploded[0]);
-    $hora_partes = explode(":", $data_voto_exploded[1]);
     ?>
     <script language="javascript">
-        atualizaContador(
-            <?php echo $row->id; ?>,
-            <?php echo $data_partes[0]; ?>,
-            <?php echo $data_partes[1]; ?>,
-            <?php echo $data_partes[2]; ?>,
-            <?php echo $hora_partes[0]; ?>,
-            <?php echo $hora_partes[1]; ?>,
-            <?php echo $hora_partes[2]; ?>
-        );
+        atualizaContador(<?php echo $row->id; ?>,<?php echo date('Y', strtotime($next_vote_time)); ?>,<?php echo date('m', strtotime($next_vote_time)); ?>,<?php echo date('d', strtotime($next_vote_time)); ?>,<?php echo date('H', strtotime($next_vote_time)); ?>,<?php echo date('i', strtotime($next_vote_time)); ?>,<?php echo date('s', strtotime($next_vote_time)); ?>);
     </script>
-    <div style="background:url(images/buttons/<?php echo $row->top_img; ?>); background-repeat: no-repeat; background-size: 87px 47px; width:87px; height:47px; border:1px solid #999; margin-top:5px; margin-left:5px; float:left;">
-        <div style="width:87px; height:47px; font-size:10px; font-family:Arial; background: rgba(0,0,0,0.7); text-shadow:1px 1px #000; font-weight:bold; color: #fff; text-align: center;">
-            <?php echo $language_05; ?><br>
-            <font size="3"><span id="contador<?php echo $row->id; ?>"></span></font><br>
-            <?php echo $language_06; ?>
+    <div style='background:url(images/buttons/<?php echo $row->top_img; ?>); background-repeat: no-repeat; background-size: 87px 47px; width:87px; height:47px; border:1px solid #999; margin-top:5px; margin-left:5px; float:left;'>
+        <div style='width:89px; *width:87px; _width:87px; height:49px; *height:47px; _height:47px; font-size:10px; font-family:Arial; background: rgba(0,0,0,0.7); text-shadow:1px 1px #000; font-weight:bold;'>
+            <?php echo $language_05; ?><br><font size='3'><span id='contador<?php echo $row->id; ?>'></span></font><br><?php echo $language_06; ?>
         </div>
     </div>
     <?php
--- painel/tops/itopz.php
+++ painel/tops/itopz.php
@@ -9,38 +9,84 @@
 //       Brazillian Developer / WebSite: http://www.icpfree.com.br       \\
 //                 Email & Skype: ivan1507@gmail.com.br                  \\
 //=======================================================================\\
-if(@fsockopen(str_replace("https://","",str_replace("http://","",$row->top_url)), 80 , $errno , $errstr , 30)){
-    @header('Content-Type: text/html; charset=utf-8');
-    // https://itopz.com/check/DEMO/39/1.1.1.1 (works random)
-    // DEMO = api key
-    // 39 = server id
-    // 1.1.1.1 = user ip
-    $pagina = @file_get_contents("https://itopz.com/check/".$row->top_token."/".$row->top_id."/".get_client_ip());
-    $pagina = json_decode($pagina);
-    $data_modificada = isset($pagina->voteTime) ? $pagina->voteTime : time();
-    $data_modificada = date("Y-m-d H:i:s", strtotime('+12 hours', $data_modificada));
-    if(strtotime($data_modificada) >= strtotime(date('Y-m-d H:i:s'))){
-        $data_voto = explode("-", substr(str_replace(" ", "", $data_modificada), 0, 10));
-        $hora_voto = explode(":", substr(str_replace(" ", "", $data_modificada), 10, 19));
-        $tops_voted = array_replace($tops_voted, array($i => array(1, $data_modificada)));
-        ?>
-        <script language="javascript">
-            atualizaContador(<?php echo $row->id; ?>,<?php echo $data_voto[0]; ?>,<?php echo $data_voto[1]; ?>,<?php echo $data_voto[2]; ?>,<?php echo $hora_voto[0]; ?>,<?php echo $hora_voto[1]; ?>,<?php echo $hora_voto[2]; ?>);
-        </script>
-        <div style='background:url(images/buttons/<?php echo $row->top_img; ?>); background-repeat: no-repeat; background-size: 87px 47px; width:87px; height:47px; border:1px solid #999; margin-top:5px; margin-left:5px; float:left;'>
-            <div style='width:89px; *width:87px; _width:87px; height:49px; *height:47px; _height:47px; font-size:10px; font-family:Arial; background: rgba(0,0,0,0.7); text-shadow:1px 1px #000; font-weight:bold;'>
-                <?php echo $language_05; ?><br><font size='3'><span id='contador<?php echo $row->id; ?>'></span></font><br><?php echo $language_06; ?>
-            </div>
-        </div>
-        <?php
-    }else{
-        ?>
-        <div style='width:87px; height:47px; border:1px solid #999; margin-top:5px; margin-left:5px; float:left;'>
-            <a href='https://itopz.com/vote/<?php echo $row->top_id; ?>' target='_blank'><img src='images/buttons/<?php echo $row->top_img; ?>' title='Vote our server on iTopz.com' border='0' width='87' height='47'"></a>
-        </div>
-        <?php
+//				      4TeamBR https://4teambr.com/						 \\
+
+// Definir URL da API iTopz
+$URL = "https://itopz.com/check/{$row->top_token}/{$row->top_id}/" . get_client_ip();
+
+// Inicializar cURL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $URL);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'curl/7.68.0 ICPNetwork/2.8');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+// Executar a requisição e obter resposta
+$response = curl_exec($ch);
+$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+$can_vote = false;
+$tops_voted = array_replace($tops_voted, array($i => array(1, '0000-00-00 00:00:00')));
+
+if ($response === false || $http_status != 200) {
+    // Se a API não responder, permitir o voto como fallback
+    $can_vote = true;
+} else {
+    $json = json_decode($response);
+
+    if ($json !== null && isset($json->voteTime)) {
+        // Capturar o fuso horário do servidor
+        $timezone_server = date_default_timezone_get();
+
+        // Configurar UTC para processar voteTime corretamente
+        date_default_timezone_set('Europe/Athens');
+
+        // Converter voteTime (Unix Timestamp) para UTC
+        $vote_time_utc = date("Y-m-d H:i:s", $json->voteTime);
+
+        // Ajustar para o fuso horário do servidor
+        date_default_timezone_set($timezone_server);
+        $vote_time_local = date("Y-m-d H:i:s", strtotime($vote_time_utc));
+
+        // Somar 12 horas ao horário local do voto
+        $next_vote_time = date("Y-m-d H:i:s", strtotime($vote_time_local . " +12 hours"));
+
+        // Capturar o horário atual no fuso do servidor
+        $current_time = date("Y-m-d H:i:s");
+
+        // Verificar se o tempo atual já passou do próximo voto permitido
+        if ($current_time >= $next_vote_time) {
+            $can_vote = true;
+        } else {
+            $can_vote = false;
+        }
+    } else {
+        $can_vote = true;
     }
-}else{
-    $tops_voted = array_replace($tops_voted, array($i => array(1, '0000-00-00 00:00:00')));
 }
+
+// Atualizar o array de votos com o estado atual
+$tops_voted = array_replace($tops_voted, array($i => array($can_vote ? 1 : 0, date('Y-m-d H:i:s'))));
+
+// Exibir botão ou contador de tempo restante
+if ($can_vote):
+    ?>
+    <div style='width:87px; height:47px; border:1px solid #999; margin-top:5px; margin-left:5px; float:left;'>
+        <a href='https://itopz.com/vote/<?php echo $row->top_id; ?>' target='_blank'><img src='images/buttons/<?php echo $row->top_img; ?>' title='Vote our server on iTopz.com' border='0' width='87' height='47'></a>
+    </div>
+    <?php
+else:
+    ?>
+    <script language="javascript">
+        atualizaContador(<?php echo $row->id; ?>,<?php echo date('Y', strtotime($next_vote_time)); ?>,<?php echo date('m', strtotime($next_vote_time)); ?>,<?php echo date('d', strtotime($next_vote_time)); ?>,<?php echo date('H', strtotime($next_vote_time)); ?>,<?php echo date('i', strtotime($next_vote_time)); ?>,<?php echo date('s', strtotime($next_vote_time)); ?>);
+    </script>
+    <div style='background:url(images/buttons/<?php echo $row->top_img; ?>); background-repeat: no-repeat; background-size: 87px 47px; width:87px; height:47px; border:1px solid #999; margin-top:5px; margin-left:5px; float:left;'>
+        <div style='width:89px; *width:87px; _width:87px; height:49px; *height:47px; _height:47px; font-size:10px; font-family:Arial; background: rgba(0,0,0,0.7); text-shadow:1px 1px #000; font-weight:bold;'>
+            <?php echo $language_05; ?><br><font size='3'><span id='contador<?php echo $row->id; ?>'></span></font><br><?php echo $language_06; ?>
+        </div>
+    </div>
+    <?php
+endif;
 ?>
